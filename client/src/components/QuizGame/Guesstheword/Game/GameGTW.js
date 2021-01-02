63f90c8eee1c3e411c8fc9ff43fc056698419115
Mansi Madhushree <68467440+mmkm10@users.noreply.github.com>
2021-01-02 19:45:37 +0530

    styling changes
    delete old files

--- client/src/components/QuizGame/Guesstheword/Game/GameGTW.js
+++ client/src/components/QuizGame/Guesstheword/Game/GameGTW.js
@@ -21,10 +21,6 @@ class GameGTW extends Component {
         setTimeout('this.preback', 0);
     }
 
-    componentDidMount(){
-        window.scrollTo(0, 0);
-    }
-
     preback() {
         window.history.forward();
     }
@@ -97,52 +93,56 @@ class GameGTW extends Component {
 
         return (
             <div>
-            <Navbar />
-                <div className="game-gtw-container">
-                    <div id="game" className="justify-center flex-column">
-                        <div id="hud">
-                            <div className="hud-item">
-                                <p id="progressText" className="hud-prefix">
-                                    Time Left: {this.state.timeLeft}s
-                    </p>
-                            </div>
-                            <div className="hud-item">
-                                <p className="hud-prefix">
-                                    Score
-                    </p>
-                                <h1 className="hud-main-text" id="score">
-                                    {this.state.score}
-                                </h1>
-                            </div>
-                        </div>
-                        {/* <div className='time_line'>.</div> */}
-                        <h1 className="" id="question">{this.state.currentQuestion.question}</h1>
-                        <div className="quiz-gtw-content-flex">
-                        <div>
-                        <img src={this.state.currentQuestion.imgSrc} className="choice-image"/>
-                        </div>
-                        <div className="quiz-gtw-options-flex">
-                        <div className="choice-container">
-                            <p className="choice-prefix">A</p>
-                            <p className="choice-text" data-number="1" onClick={this.checkAnswer}>{this.state.currentQuestion.choice1}</p>
-                        </div>
-                        <div className="choice-container">
-                            <p className="choice-prefix">B</p>
-                            <p className="choice-text" data-number="2" onClick={this.checkAnswer}>{this.state.currentQuestion.choice2}</p>
-                        </div>
-                        <div className="choice-container">
-                            <p className="choice-prefix">C</p>
-                            <p className="choice-text" data-number="3" onClick={this.checkAnswer}>{this.state.currentQuestion.choice3}</p>
-                        </div>
-                        <div className="choice-container">
-                            <p className="choice-prefix">D</p>
-                            <p className="choice-text" data-number="4" onClick={this.checkAnswer}>{this.state.currentQuestion.choice4}</p>
-                        </div>
-                        </div>
-                        </div>
-                        
-                    </div>
+                <Navbar />
+                <div className="gtw-pic">
+                    <div className="game-gtw-container">
 
+                        <div id="game" className="justify-center flex-column">
+
+                            <div id="hud">
+                                <div className="hud-item">
+                                    <p id="progressText" className="hud-prefix">
+                                        Time Left: {this.state.timeLeft}s
+                    </p>
+                                </div>
+                                <div className="hud-item">
+                                    <p className="hud-prefix">
+                                        Score
+                    </p>
+                                    <h1 className="hud-main-text" id="score">
+                                        {this.state.score}
+                                    </h1>
+                                </div>
+                            </div>
+                            {/* <div className='time_line'>.</div> */}
+                            <h1 className="" id="question">{this.state.currentQuestion.question}</h1>
+                            <div className="quiz-gtw-content-flex">
+
+                                <div>
+                                    <img src={this.state.currentQuestion.imgSrc} className="choice-image" />
+                                </div>
+                                <div className="quiz-gtw-options-flex">
+                                    <div className="choice-containers">
+                                        <p className="choice-prefix">A</p>
+                                        <p className="choice-text" data-number="1" onClick={this.checkAnswer}>{this.state.currentQuestion.choice1}</p>
+                                    </div>
+                                    <div className="choice-containers">
+                                        <p className="choice-prefix">B</p>
+                                        <p className="choice-text" data-number="2" onClick={this.checkAnswer}>{this.state.currentQuestion.choice2}</p>
+                                    </div>
+                                    <div className="choice-containers">
+                                        <p className="choice-prefix">C</p>
+                                        <p className="choice-text" data-number="3" onClick={this.checkAnswer}>{this.state.currentQuestion.choice3}</p>
+                                    </div>
+                                    <div className="choice-containers">
+                                        <p className="choice-prefix">D</p>
+                                        <p className="choice-text" data-number="4" onClick={this.checkAnswer}>{this.state.currentQuestion.choice4}</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         );
