a1e91e2c050aea6de2c724e989b19f6154d866d9
Mansi Madhushree <68467440+mmkm10@users.noreply.github.com>
2021-01-02 19:48:18 +0530

    style changes
    edit/delete the old files

--- client/src/components/QuizGame/Rules/Rules.js
+++ client/src/components/QuizGame/Rules/Rules.js
@@ -2,40 +2,41 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../../Navbar/Navbar';
 import './Rules.css';
+import pic from './gbrules.png';
 
 class Rules extends Component {
 
 
-  constructor(props){
+  constructor(props) {
     super(props)
-    setTimeout(() => {window.history.forward()}, 0);  
-  }
-
-  componentDidMount(){
-    window.scrollTo(0, 0);
+    setTimeout(() => { window.history.forward() }, 0);
   }
 
   render() {
     return (
       <>
-      <Navbar />
-      <div className="quiz-rules-container">
-        <div id="rules-bg" className='rule'>
-          <div className="info-title"><span className="rules_heading">Some Rules of this Quiz</span></div>
-          <div className="info-list">
-            <div className="info">1. You will have only <span className="ruleTime">60 seconds</span> total.</div>
-            <div className="info">2. Once you select your answer, it can't be undone.</div>
-            <div className="info">3. You can't exit from the Quiz while you're playing.</div>
-            <div className="info">4. You'll get 10 points on each of your correct answers.</div>
-          </div>
-          <div className="rules-buttons">
-            <Link to='/game' className="quit">Exit Quiz</Link>
-            <Link to='/game/quiz/guesstheword/play' className="continue">Guess the word Quiz</Link>
-            <Link to='/game/quiz/gk/play' className="continue">GK Quiz</Link>
-            <Link to='/game/quiz/meaning/play' className="continue">Word Meaning</Link>
+        <Navbar />
+        
+          <div className="quiz-rules-container" >
+          <div className="game-rules-pic">
+            <div className='rule' >
+            
+              <div className="info-title"><span className="rules_heading">Some Rules of this Quiz</span></div>
+              <div className="info-list">
+                <div className="info">1. You will have only <span className="ruleTime">60 seconds</span> total.</div>
+                <div className="info">2. Once you select your answer, it can't be undone.</div>
+                <div className="info">3. You can't exit from the Quiz while you're playing.</div>
+                <div className="info">4. You'll get 10 points on each of your correct answers.</div>
+              </div>
+              <div className="rules-buttons">
+                <Link to='/' className="quit">Exit Quiz</Link>
+                <Link to='/game/quiz/guesstheword/play' className="continue">Guess the word Quiz</Link>
+                <Link to='/game/quiz/gk/play' className="continue">GK Quiz</Link>
+                <Link to='/game/quiz/meaning/play' className="continue">Word Meaning</Link>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
       </>
     );
   }
